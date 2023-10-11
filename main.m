8300619d6d642ab59b14ebafbd6ea69186383550
MANAV AGARWAL <94757193+Manav321@users.noreply.github.com>
2023-10-11 19:55:28 +0530

    updated with solution

--- main.m
+++ main.m
@@ -1,3 +1,5 @@
+%clear all;
+
 % order of matrix nxn
 n = 20; 
 
@@ -5,6 +7,12 @@ n = 20;
 mat_A = zeros(n, n);
 mat_B = zeros(n ,1);
 mat_X = zeros(n, 1);
+
+R_rms = 1;
+R = zeros(1, n);
+w = 1; % relaxation factor
+thresshold = 1e-6;
+E = []; % array to store R_rms to plot
 
 % Inputting values of matrix A
 for i=1:n
@@ -27,3 +35,30 @@ end
 
 % Inputting values in matrix B
 mat_B(n,1) = 1;
+
+
+while R_rms > thresshold
+
+    R_rms = 0.0;
+    
+    for i = 1:n
+   
+        if i == 1
+            R(i) = mat_B(i) - (2*mat_X(i) - mat_X(i+1));
+        elseif i == n
+            R(i) = mat_B(i) - (-mat_X(i-1) + 2*mat_X(i));
+        else
+            R(i) = mat_B(i) - (-mat_X(i-1) + 2*mat_X(i) - mat_X(i+1));
+        end
+
+        mat_X = mat_X + (w*R(i))/2;
+        R_rms = R_rms + R(i)^2;
+        
+    end
+    
+    R_rms = sqrt(R_rms/n);
+    E = [E, R_rms];
+
+    plot(E, '-r*');
+    drawnow;
+end
